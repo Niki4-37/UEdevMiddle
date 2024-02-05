a8f8c8be514848ccfb8154b465353a99eaf79b87
Vladimir <redcarpet@mail.ru>
2024-02-05 13:02:12 +0300

    homework_1_3

--- UEdevMiddle/Vector.h
+++ UEdevMiddle/Vector.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <cmath>
 
 class Vector
 {
@@ -8,7 +9,44 @@ public:
     Vector(float x, float y, float z) : x{ x }, y{ y }, z{ z } {};
 
     friend Vector operator+(const Vector& v1, const  Vector& v2);
+    friend Vector operator-(const Vector& v1, const  Vector& v2);
     friend std::ostream& operator<<(std::ostream& out, const Vector& v);
+    friend std::istream& operator>>(std::istream& in, Vector& inputVector);
+
+    float operator[](int index)
+    {
+        switch (index)
+        {
+        case 0:
+            return x;
+            break;
+        case 1:
+            return y;
+            break;
+        case 2:
+            return z;
+            break;
+        default:
+            std::cout << "error, wrong index";
+            return 0.f;
+        }
+    }
+
+    operator float()
+    {
+        std::cout << "vector lenght ";
+        return sqrt(x * x + y * y + z * z);
+    }
+
+    Vector operator*(int modifier)
+    {
+        return Vector(x * modifier, y * modifier, z * modifier);
+    }
+
+    Vector operator-()
+    {
+        return Vector(-x, -y, -z);
+    }
 
 private:
     float x, y, z;
@@ -19,8 +57,24 @@ Vector operator+(const Vector& v1, const  Vector& v2)
     return Vector(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
 }
 
+Vector operator-(const Vector& v1, const  Vector& v2)
+{
+    return Vector(v1.x - v2.x, v1.y - v2.y, v1.z - v2.z);
+}
+
 std::ostream& operator<<(std::ostream& out, const Vector& v)
 {
     out << v.x << ' ' << v.y << ' ' << v.z;
     return out;
+}
+
+std::istream& operator>>(std::istream& in, Vector& inputVector)
+{
+    std::cout << "x value: ";
+    in >> inputVector.x;
+    std::cout << "y value: ";
+    in >> inputVector.y;
+    std::cout << "z value: ";
+    in >> inputVector.z;
+    return in;
 }
--- UEdevMiddle/main.cpp
+++ UEdevMiddle/main.cpp
@@ -5,6 +5,8 @@ int main()
 {
     Vector v1(3.f, 0.5f, 4.5f);
     Vector v2(1.5f, 3.f, 1.5f);
-    std::cout << v1 + v2;
+    Vector v3;
+    std::cin >> v3;
+    std::cout << v3 - v1;
     return 0;
 }
